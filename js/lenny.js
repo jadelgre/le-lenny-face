181415f625cb8e57bc12907a36835ad7e5ed1965
Justin Leniger <jtleniger@gmail.com>
2015-08-15 13:48:51 -0600

    Added draw method

--- js/lenny.js
+++ js/lenny.js
@@ -37,7 +37,11 @@
  	Lenny.VERSION = '0.0.1';
 
  	Lenny.DEFAULTS = {
- 		font: "100px Arial",
+ 		fontName: "Arial",
+ 		fontHeightPixels: 100,
+ 		color: "#000000",
+ 		canvasWidth: 800,
+ 		canvasHeight: 600,
  		text: "( ͡° ͜ʖ ͡°)"
  	};
 
@@ -49,6 +53,10 @@
  		}
 
  		_this.options = $.extend({}, Lenny.DEFAULTS, options);
+
+
+ 		_this.fontString = _this.options.fontHeightPixels.toString() + "px " + _this.options.fontName;
+
  		_this.$el = $(element);
  		_this.ctx = _this.$el[0].getContext("2d");
  		_this.startAnimation();
@@ -57,10 +65,28 @@
  	Lenny.prototype.startAnimation = function () {
  		var _this = this;
 
- 		_this.ctx.font = _this.options.font;
- 		_this.ctx.fillStyle = "#000000";
- 		_this.ctx.fillText(_this.options.text, 290, 280);
+ 		_this.drawFace(_this.options.canvasWidth / 2, _this.options.canvasHeight / 2);
  	};
+
+ 	// Draws the face, centered on x/y coordinates, positive left and down from top left corner
+ 	Lenny.prototype.drawFace = function(x, y) {
+
+ 		if (typeof (x) === 'undefined' || typeof (y) === 'undefined') {
+ 			return;
+ 		}
+
+ 		var _this = this;
+
+ 		_this.ctx.font = _this.fontString;
+
+ 		var width = _this.ctx.measureText(_this.options.text).width;
+
+ 		// Adjust so the face falls in the middle of the x/y coords
+ 		x = x - (width / 2);
+
+ 		_this.ctx.fillStyle = _this.options.color;
+ 		_this.ctx.fillText(_this.options.text, x, y);
+ 	}
 
  	// LENNY PlUGIN DEFINITION
  	// =======================
