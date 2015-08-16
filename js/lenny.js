003a7b439d245019ccd690dfff506ba0c2db4a4a
Justin Leniger <jtleniger@gmail.com>
2015-08-16 11:57:07 -0600

    Slight refactor; physics implemented for bounce mode:

--- js/lenny.js
+++ js/lenny.js
@@ -36,13 +36,16 @@
 
  	Lenny.VERSION = '0.0.1';
 
+ 	Lenny.MODES = ['bounce', 'random'];
+
  	Lenny.DEFAULTS = {
  		fontName: "Arial",
  		fontHeightPixels: 100,
  		color: "#000000",
  		canvasWidth: 800,
  		canvasHeight: 600,
- 		text: "( ͡° ͜ʖ ͡°)"
+ 		text: "( ͡° ͜ʖ ͡°)",
+ 		animationType: Lenny.MODES[0]
  	};
 
  	Lenny.prototype._init = function (element, options) {
@@ -53,48 +56,122 @@
  		}
 
  		_this.options = $.extend({}, Lenny.DEFAULTS, options);
-
+ 		_this.modes = Lenny.MODES;
+ 		_this.physics = {};
 
  		_this.fontString = _this.options.fontHeightPixels.toString() + "px " + _this.options.fontName;
 
+ 		_this.intervalIDs = [];
+
  		_this.$el = $(element);
+
  		_this.ctx = _this.$el[0].getContext("2d");
+ 		_this.ctx.font = _this.fontString;
+ 		_this.ctx.fillStyle = _this.options.color;
+
  		_this.startAnimation();
  	};
 
- 	Lenny.prototype.startAnimation = function () {
+ 	Lenny.prototype.addInterval = function (intervalFunction, interval) {
  		var _this = this;
-		_this.randomFace(200);
-		_this.slideFromLeft();
- 		//_this.drawFace(_this.options.canvasWidth / 2, _this.options.canvasHeight / 2);
+
+ 		_this.intervalIDs.push(setInterval(intervalFunction, interval));
  	};
 
-	Lenny.prototype.slideFromLeft = function () {
+ 	Lenny.prototype.addTimeout = function (timeoutFunction, timeout) {
+ 		var _this = this;
+
+ 		_this.intervalIDs.push(setTimeout(timeoutFunction, timeout));
+ 	}
+
+ 	Lenny.prototype.animateBounce = function () {
+ 		var _this = this;
+
+ 		_this.physics.V_X = 0.4;
+ 		_this.physics.V_Y = 0;
+
+ 		// In the canvas coordinate system, down is positive
+ 		_this.physics.A_Y = 0.0121;
+ 		_this.physics.A_X = 0;
+
+ 		_this.physics.X = _this.getTextWidth();
+ 		_this.physics.Y = _this.options.fontHeightPixels;
+
+ 		var millisPerFrame = 17;
+
+ 		_this.addInterval(function() {
+ 			_this.clearCanvas();
+ 			_this.physics_updatePosition(millisPerFrame);
+ 			_this.drawFace(_this.physics.X, _this.physics.Y);
+ 		}, millisPerFrame);
+
+ 	};
+
+ 	Lenny.prototype.physics_updatePosition = function (timeSlice) {
+ 		var physics = this.physics;
+ 		var _this = this;
+
+ 		console.log(physics);
+
+ 		physics.X = (0.5 * physics.A_X * Math.pow(timeSlice, 2)) + (physics.V_X * timeSlice) + physics.X; // x(t) = 1/2at^2 + vt + x0
+ 		physics.Y = (0.5 * physics.A_Y * Math.pow(timeSlice, 2)) + (physics.V_Y * timeSlice) + physics.Y;
+
+ 		physics.V_X = (physics.A_X * timeSlice) + physics.V_X;
+ 		physics.V_Y = (physics.A_Y * timeSlice) + physics.V_Y;
+
+ 		var rightEdge = physics.X + (_this.getTextWidth() / 2);
+ 		var leftEdge = physics.X - (_this.getTextWidth() / 2); 
+
+ 		if (rightEdge > _this.options.canvasWidth || leftEdge < 0) {
+ 			physics.V_X = -1 * physics.V_X;
+ 		}
+
+ 		if (physics.Y >= _this.options.canvasHeight || physics.Y <= 0) {
+ 			physics.V_Y = -1 * physics.V_Y;
+ 		}
+ 	};
+
+ 	Lenny.prototype.animateRandom = function (interval) {
 		var _this = this;
+		
+		_this.addInterval(function() { 
+			_this.clearCanvas();			
+			// draw a new face at random x & y coords that are within canvas bounds
+			_this.drawFace(Math.random() * _this.options.canvasWidth, Math.random() * _this.options.canvasHeight);
+		}, interval);
+	};
+
+	Lenny.prototype.animateSlideFromLeft = function () {
+		var _this = this;
+
 		var move = function(x, y) {
-			setTimeout(function() {
-				_this.ctx.clearRect(0,0,_this.options.canvasWidth,_this.options.canvasHeight);
-				_this.drawFace( x, y)
-			}, 3*x);
+			_this.addTimeout(function() {
+				_this.clearCanvas();
+				_this.drawFace(x, y);
+			}, 3 * x);
 		};
-		for(var i = 0; i < _this.options.canvasWidth / 2; i++ ) {
+
+		for (var i = 0; i < _this.options.canvasWidth / 2; i++ ) {
 			move(i, _this.options.canvasHeight / 2);	
 		}
 	};
 
-	Lenny.prototype.randomFace = function(interval) {
+	Lenny.prototype.clearCanvas = function () {
 		var _this = this;
-		setInterval(function() { 
-			// clear the canvas
-			_this.ctx.clearRect(0,0,_this.options.canvasWidth,_this.options.canvasHeight);
-			// draw a new face at random x & y coords that are within canvas bounds
-			_this.drawFace(Math.random()*_this.options.canvasWidth,Math.random()*_this.options.canvasHeight);
-		},interval);
-	}
 
+		_this.ctx.clearRect(0, 0, _this.options.canvasWidth, _this.options.canvasHeight);
+	};
 
- 	// Draws the face, centered on x/y coordinates, positive left and down from top left corner
- 	Lenny.prototype.drawFace = function(x, y) {
+	Lenny.prototype.clearIntervals = function () {
+		var _this = this;
+
+		$.each(_this.intervalIDs, function(key, value) {
+			clearInterval(value);
+		});
+	};
+
+	// Draws the face, centered on x/y coordinates, positive left and down from top left corner
+ 	Lenny.prototype.drawFace = function (x, y) {
 
  		if (typeof (x) === 'undefined' || typeof (y) === 'undefined') {
  			return;
@@ -102,17 +179,47 @@
 
  		var _this = this;
 
- 		_this.ctx.font = _this.fontString;
-
- 		var width = _this.ctx.measureText(_this.options.text).width;
+ 		var width = _this.getTextWidth();
 
  		// Adjust so the face falls in the middle of the x/y coords
  		x = x - (width / 2);
 
- 		_this.ctx.fillStyle = _this.options.color;
  		_this.ctx.fillText(_this.options.text, x, y);
- 	}
+ 	};
 
+ 	Lenny.prototype.getTextWidth = function () {
+ 		var _this = this;
+
+ 		return _this.ctx.measureText(_this.options.text).width;
+ 	};
+
+ 	Lenny.prototype.setMode = function (mode) {
+ 		var _this = this;
+
+ 		if ($.inArray(mode, _this.modes) !== -1) {
+ 			_this.clearIntervals();
+ 			_this.clearCanvas();
+ 			_this.options.animationType = mode;
+ 			_this.startAnimation();
+ 		}
+ 	};
+
+	Lenny.prototype.startAnimation = function () {
+ 		var _this = this;
+
+		switch (_this.options.animationType) {
+			case 'bounce':
+				_this.animateBounce();
+				break;
+			case 'random':
+				_this.animateSlideFromLeft();
+				_this.animateRandom(200);
+				break;
+			default:
+				_this.animateRandom(200);
+				break;
+		}
+ 	};
 	
  	// LENNY PlUGIN DEFINITION
  	// =======================
--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 +function() {
-	$('#lenny').lenny();
+	var $canvas = $('#lenny');
+
+	$canvas.lenny();
+
+	var modes = $canvas.data('lenny').modes;
+
+	$.each(modes, function(key, value) {
+		$('#mode-select')
+		.append($("<option></option>")
+		.attr("value", value)
+		.text(value));
+	});
+
+	$('#mode-select').change(function() {
+		$canvas.data('lenny').setMode($('#mode-select').find(':selected').val());
+	});
 }();
